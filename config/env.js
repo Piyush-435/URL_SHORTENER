9b3e38b9004ad5dfa64d51751fa31b80ba82f4db
Piyush-435 <piyushchand808@gmail.com>
2026-02-23 19:36:00 +0530

    fix: remove GITHUB_CALLBACK_URL from zod schema

--- config/env.js
+++ config/env.js
@@ -7,7 +7,7 @@ const envSchema = z.object({
   GITHUB_CLIENT_SECRET: z.string().min(1),
   FRONTEND_URL: z.string().url().trim().min(1),
   GOOGLE_CALLBACK_URL: z.string().url().min(1), // ✅ add this
-//   GITHUB_CALLBACK_URL: z.string().url().min(1), // ✅ add this too for GitHub
+  // ✅ add this too for GitHub
 });
 
 export const env = envSchema.parse(process.env);
